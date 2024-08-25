d15077284aa8a794170b152954c2285de6e9fc68
Sto <sto@192.168.1.18>
2024-08-25 03:54:06 +0100

    [] initial commit2

---
+++ Products.php
@@ -0,0 +1,109 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+class Products {
+	private static $instance;
+	private static $mysqli_conn;
+	
+	public function __construct() {
+	}
+	
+	public static function get_products() {
+		header("Content-Type: application/json; charset=UTF-8");
+		$posted = file_get_contents("php://input");
+		if (!$posted || !($obj = json_decode($posted, false)) || ($obj->start_from ?? '')==='' || !is_numeric($obj->start_from)) {
+			die(json_encode(['error'=>'wrong params']));
+		}
+		$order_by = null;
+		if (($obj->order_by ?? '')!='') {
+			switch ($obj->order_by) {
+				case 'ther_sorting_byprice':
+					$order_by = 'price';
+				break;
+				case 'ther_sorting_byreview':
+					$order_by = 'reviews';
+				break;
+				case 'ther_sorting_byname':
+					$order_by = '`name`';
+				break;
+				case 'ther_sorting_bysaving':
+					$order_by = 'was_price-price';
+				break;
+			}
+		}
+		$res = self::get_mysqli_conn()->prepare("SELECT name,price,was_price,reviews,img FROM products".($order_by ? ' ORDER BY '.$order_by : '')." LIMIT ?,12");
+		$res->bind_param("i", $obj->start_from);
+		$res->execute();
+		$products = $res->get_result()->fetch_all(MYSQLI_ASSOC);
+		$next_batch_count = 0;
+		if (count($products)==12) {
+			$res = self::get_mysqli_conn()->prepare("SELECT COUNT(product_id) as countt FROM products");
+			$res->execute();
+			$next_batch_count=($next_batch_count=($res->get_result()->fetch_column() - $obj->start_from)) > 12 ? 12 : ($next_batch_count < 0 ? 0 : $next_batch_count);
+		}
+		die(json_encode(['next_batch_count'=>$next_batch_count,'product_arr'=>$products]));
+	}
+	
+	public static function handle_compatibility_version($version):void {
+		if ($version == '1.0.0') {
+			$mysqli=self::get_mysqli_conn();
+			$sql = "CREATE TABLE IF NOT EXISTS products(
+				product_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+				name VARCHAR(255) NOT NULL,
+				price DECIMAL(6,2) NOT NULL,
+				was_price DECIMAL(6,2) DEFAULT NULL,
+				reviews TINYINT UNSIGNED DEFAULT NULL,
+				img INT UNSIGNED DEFAULT NULL,
+				PRIMARY KEY (product_id)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+			if (!$mysqli->query($sql) || $mysqli->errno) {
+			   printf("Could not create table: %s", $mysqli->error);exit;
+			}
+			$mysqli->close();
+		}
+	}
+	public static function populate_products():void {
+		$mysqli=self::get_mysqli_conn();
+		$img_id=0;
+		$prepared = $mysqli->prepare("INSERT INTO products (name,price,was_price,reviews,img) VALUES (?,?,?,?,?)");
+		if($prepared==false) die('$prepared failed - '.$i);
+		for ($i=1;$i<=5000;$i++) {
+			$img_id++;
+			$name = "Product $i";
+			$price = $i+($i%3==0 ? 0.99 : 0);
+			$was_price = $i%2==0 ? $i+mt_rand(1,5) : null;
+			$reviews = $i%2!=0 ? mt_rand(1,99) : null;
+			$result=$prepared->bind_param("sddii",$name,$price,$was_price,$reviews,$img_id);
+			if($result==false) die('bind_param failed - '.$i);
+			
+			$result=$prepared->execute();
+			if($result==false) die('execute failed - '.$i);
+			$prepared->close();
+			if ($img_id==12) $img_id=0;
+		}
+		$mysqli->close();
+		die('done');
+	}
+	
+	/** mysqli_conn instance */
+	private static function get_mysqli_conn():mysqli {
+		if ( ! isset( self::$mysqli_conn ) ) {
+			self::$mysqli_conn = new mysqli("localhost", "root", "", "the_range_ecommerce");
+			if(self::$mysqli_conn->connect_errno ) {
+				printf("mysqli connection failed: %s", self::$mysqli_conn->connect_error);exit;
+			 }
+		}
+		return self::$mysqli_conn;
+	}
+	/** Singleton instance */
+	public static function get_instance():self {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+	
+}
+//Products::handle_compatibility_version('1.0.0');
+//Products::populate_products();
+Products::get_products();
---
+++ js/app.js
@@ -0,0 +1,93 @@
+window.onload = ()=>{
+	const fetch_the_products = async (start_from=0) => {
+		start_from = parseInt(start_from);
+		if (start_from==0) {
+			document.getElementById('the_range_products').innerHTML='';
+		}
+		const ther_loaddata_from = document.getElementById('ther_loaddata_from').value, ther_loadmore_button_dom=document.getElementById('ther_loadmore_button'), ther_sortby_selected = document.getElementById('ther_sortby_selected').value;
+		ther_loadmore_button_dom.childNodes[1].style.display='inline-block'; //show the spinner
+		const resp = await fetch(ther_loaddata_from==1 ? 'data/product.json' : 'Products.php', {
+			method: 'POST',
+			headers: {
+			  'Accept': 'application/json',
+			  'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({start_from: start_from, order_by:ther_sortby_selected})
+		});
+		const the_json = await resp.json();
+		//if we fetch from the json file order here, otherwise the order was in the backend
+		if (ther_loaddata_from==1 && the_json.product_arr.length) {
+			switch(ther_sortby_selected) {
+				case 'ther_sorting_byprice':
+					the_json.product_arr.sort(function(a, b){return a.price - b.price;});
+				break;
+				case 'ther_sorting_byreview':
+					the_json.product_arr.sort(function(a, b){return a.reviews - b.reviews;});
+				break;
+				case 'ther_sorting_byname':
+					the_json.product_arr.sort(function(a, b) {
+						let x = a.name.toLowerCase();
+						let y = b.name.toLowerCase();
+						if (x < y) {return -1;}
+						if (x > y) {return 1;}
+						return 0;
+					});
+				break;
+				case 'ther_sorting_bysaving':
+					//add a new property to the_json - saving, which will be was_price - price. Then order by this new property.
+					for (let i=0;i<the_json.product_arr.length;i++) {
+						the_json.product_arr[i]['saving']=the_json.product_arr[i].was_price ? the_json.product_arr[i].was_price - the_json.product_arr[i].price : 0;
+					}
+					the_json.product_arr.sort(function(a, b){return a.saving - b.saving;});
+				break;
+			}
+		}
+		
+		ther_loadmore_button_dom.childNodes[1].style.display='none'; //hide the spinner
+		ther_loadmore_button_dom.style.opacity = the_json.next_batch_count==0 ? '25%' : '100%';
+		ther_loadmore_button_dom.disabled = the_json.next_batch_count==0; //disable the Load More button if there are no more products
+		if (the_json.next_batch_count > 0)
+		document.getElementById('start_fromm').value=start_from+12; //prepare the start_fromm for the next load if any
+	
+		build_products_html(the_json.product_arr);
+	};
+	
+	const build_products_html = (the_json) => {
+		if (!the_json.length) {
+			return;
+		}
+		
+		let the_html = '<div class="row">';
+		for (let i=0;i<the_json.length;i++) {
+			the_html += '<div class="col-md-3"><div class="innerrr the_rangeproduct">\n\
+			<div class="the_rangeproduct_img"><img src="'+(the_json[i].img ? 'img/'+the_json[i].img+'.jpg' : 'https://commercial.bunn.com/img/image-not-available.png')+'" /></div>\n\
+			<div class="the_rangeproduct_name">'+the_json[i].name+'</div>\n\
+			<div class="the_rangeproduct_price">&pound;'+(the_json[i].price ? Number(the_json[i].price).toFixed(2) : 'N/A')+'</div>\n\
+			<div class="the_rangeproduct_wasprice">'+(the_json[i].was_price ? 'Was <span class="the_rangeproduct_wasprice2">&pound;'+Number(the_json[i].was_price).toFixed(2)+'</span>' : '')+'</div>\n\
+			<div class="the_rangeproduct_reviews">'+(the_json[i].reviews ? the_json[i].reviews+'% Review Score' : '')+'</div>\n\
+			<div class="the_rangeproduct_addtobasket"><input type="button" value="Add To Basket" /></div>\n\
+			</div></div>';
+			if ((i+1)%4===0 && i < the_json.length-1) {
+				the_html += '</div><div class="row">';
+			}
+		}
+		the_html += '</div>';
+		document.getElementById('the_range_products').innerHTML+=the_html;
+	};
+	
+	document.getElementById('ther_loaddata_from').addEventListener("change", ()=>{
+		document.getElementById('start_fromm').value=0;
+		fetch_the_products();
+	});
+	document.getElementById('ther_loadmore_button').addEventListener("click", ()=>{
+		fetch_the_products(document.getElementById('start_fromm').value);
+	});
+	for (const i of ['ther_sorting_byprice', 'ther_sorting_byreview', 'ther_sorting_byname', 'ther_sorting_bysaving'])
+	document.getElementById(i).addEventListener("click", ()=>{
+		document.getElementById('ther_sortby_selected').value=i;
+		for (const k of ['ther_sorting_byprice', 'ther_sorting_byreview', 'ther_sorting_byname', 'ther_sorting_bysaving'])
+		document.getElementById(k).style['box-shadow']=k==i ? '0 0 20px #777' : 'none';
+		fetch_the_products();
+	});
+	fetch_the_products();
+};
